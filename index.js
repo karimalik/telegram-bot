44ab74fcc1e8f7176f30b560e02cd6bdf535407e
karimalik <me@karimdev.network>
2023-07-09 06:17:14 +0200

    add delete message function

--- index.js
+++ index.js
@@ -13,19 +13,44 @@ bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
     const username = msg.from.first_name; // recover the user's name
     const rules = [
-        '1. Respectez les autres membres du groupe.',
+        '1. Respectez les autres membres du groupe TROPIC LOVE.',
         '2. Pas de contenu offensant ou inapproprié.',
         '3. Évitez les spams ou les publicités non autorisées.',
         '4. Vous avez la possibilité d\'inbox un membre du groupe si son profil vous intéresse. ',
-        'Si vous avez des questions, n\'hésitez pas à les poser. Profitez de votre temps ici !'
+        'NB: Si vous avez des questions, n\'hésitez pas à les poser. Profitez de votre temps ici !'
     ];
 
-    let welcomeMessage = `Bonjour ${username} ! Bienvenue dans notre groupe. Veuillez lire les règles du groupe ci-dessous :`;
+    let welcomeMessage = `Bonjour ${username} ! Bienvenue dans notre groupe. Veuillez lire les règles du groupe ci-dessous : \n`;
 
     rules.forEach(rule => {
-        welcomeMessage += `\n- ${rule}`;
+        welcomeMessage += `\n \n ${rule}`;
     });
 
     bot.sendMessage(chatId, welcomeMessage);
 });
+
+//message handler
+bot.onText('message ', (msg) => {
+    const chatId = msg.chat.id;
+    const messageText = msg.text || '';
+
+    //Check whether the message contains unauthorised advertising
+    if (containsUnapproved(messageText)) {
+        //delete message
+        bot.deleteMessage(chatId, msg.message_id);
+    }
+});
+
+//Function to check whether the message contains unauthorised advertising
+function containsUnapproved(message) {
+    const keywords =  ['publicité', 'promo', 'vente', 'offre', 'rabais'];
+
+    for (const keyword of keywords) {
+        if (message.toLowerCase().includes(keyword)) {
+            return true;
+          }
+    }
+
+    return false;
+};
 
